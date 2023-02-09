79ce0f51d3407242530bb4c4001199fe98fd81a1
lyhue1991 <lyhue1991@163.com>
2023-02-09 20:27:09 +0800

    3.7.2

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/lyhue1991/torchkeras",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",

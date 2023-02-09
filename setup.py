7cdd1b80bffe86a5b0ccb665edb4a0d57d343f15
lyhue1991 <lyhue1991@163.com>
2023-02-09 20:44:20 +0800

    3.7.2

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 # -*- coding:utf-8 -*-
-import setuptools
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+import re
+from pathlib import Path
+from setuptools import find_packages, setup
 
-setuptools.setup(
+# Settings
+FILE = Path(__file__).resolve()
+PARENT = FILE.parent  # root directory
+README = (PARENT / "README.md").read_text(encoding="utf-8")
+
+def get_version():
+    file = PARENT / 'torchkeras/__init__.py'
+    return re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', file.read_text(encoding="utf-8"), re.M)[1]
+
+    
+setup(
     name="torchkeras",
-    version="3.7.2",
+    version=get_version(),
     author="PythonAiRoad, Laugh",
     author_email="lyhue1991@163.com",
     description="pytorch❤️keras",
-    long_description=long_description,
+    long_description=README,
     install_requires=[           
          #'pandas',
          #'accelerate',
@@ -22,12 +32,13 @@ setuptools.setup(
        ],
     long_description_content_type="text/markdown",
     url="https://github.com/lyhue1991/torchkeras",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    keywords="machine-learning, deep-learning, ML, DL, pytorch, torch, keras",
     python_requires='>=3.5'
 )
